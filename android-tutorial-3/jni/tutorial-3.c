5c0fc683721a82826f311c2e89463763ea3aa6ab
userName <userEmail>
2024-08-26 11:06:51 +0800

    delete debug log

--- android-tutorial-3/jni/tutorial-3.c
+++ android-tutorial-3/jni/tutorial-3.c
@@ -277,8 +277,8 @@ gst_native_init (JNIEnv * env, jobject thiz)
   SET_CUSTOM_DATA (env, thiz, custom_data_field_id, send_data);
   GST_DEBUG_CATEGORY_INIT (debug_category, "tutorial-3", 0, "Android tutorial 3");
 
-  gst_debug_set_threshold_for_name ("tutorial-3", GST_LEVEL_DEBUG);
-  gst_debug_set_default_threshold(GST_LEVEL_DEBUG);
+  gst_debug_set_threshold_for_name ("tutorial-3", GST_LEVEL_WARNING);
+//  gst_debug_set_default_threshold(GST_LEVEL_DEBUG);
 
   GST_DEBUG ("Created CustomData at %p", send_data);
   send_data->app = (*env)->NewGlobalRef (env, thiz);
@@ -408,12 +408,8 @@ gst_native_appsrc_data (JNIEnv * env, jobject thiz, jbyteArray imageData)
     if (!data)
         return;
 
-    GST_DEBUG ("gst_native_appsrc_data, appsrc: %p", data->appsrc);
-
     if (data->appsrc) {
         jsize length = (*env)->GetArrayLength(env, imageData);
-
-        GST_DEBUG ("Push image buffer to appsrc, len: %d", length);
 
         // 从Java获取byte[]数据
         jbyte *nativeData = (*env)->GetByteArrayElements(env, imageData, NULL);
